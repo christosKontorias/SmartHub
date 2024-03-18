184a7b6eb72569e0af881960d47c981640c33597
Christos Kontorias <45541430+christosKontorias@users.noreply.github.com>
2024-03-19 00:41:11 +0200

    Fix ID's & Like Post

--- app/src/main/java/com/example/smarthub/MainActivity.java
+++ app/src/main/java/com/example/smarthub/MainActivity.java
@@ -1,24 +1,30 @@
 package com.example.smarthub;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.widget.ImageView;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
 
 public class MainActivity extends AppCompatActivity {
+    ImageView likeImageView;
+    boolean isLiked = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        likeImageView = findViewById(R.id.imageNewsPost);
+
+        // Set OnClickListener to the ImageView
+        likeImageView.setOnClickListener(view -> {
+            // Toggle between the outline and filled heart images
+            if (isLiked) {
+                likeImageView.setImageResource(R.drawable.outline_heart_icon_24);
+            } else {
+                likeImageView.setImageResource(R.drawable.filled_heart_icon_24);
+            }
+            isLiked = !isLiked; // Invert the liked status
         });
     }
 }

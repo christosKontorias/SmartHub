5d291a81caae9be1fad0b20ad80183435dd8cde5
Christos Kontorias <45541430+christosKontorias@users.noreply.github.com>
2024-03-20 16:29:19 +0200

    Bottom Navigation Menu

--- app/src/main/java/com/example/smarthub/MainActivity.java
+++ app/src/main/java/com/example/smarthub/MainActivity.java
@@ -5,8 +5,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-
 import androidx.appcompat.app.AppCompatActivity;
+
 
 
 public class MainActivity extends AppCompatActivity {

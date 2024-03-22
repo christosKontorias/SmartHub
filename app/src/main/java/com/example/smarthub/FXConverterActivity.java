50c2d05ce027187f62ba765e3eac79286150e4ba
Christos Kontorias <45541430+christosKontorias@users.noreply.github.com>
2024-03-22 22:25:00 +0200

    Remove API & Update UI

--- app/src/main/java/com/example/smarthub/FXConverterActivity.java
+++ app/src/main/java/com/example/smarthub/FXConverterActivity.java
@@ -12,14 +12,6 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
-import org.json.JSONException;
-import org.json.JSONObject;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.util.ArrayList;
@@ -32,7 +24,7 @@ public class FXConverterActivity extends AppCompatActivity {
     Dialog toDialog;
     Button convertButton;
     String convertFromValue, convertToValue, conversionValue;
-    String[] country = {"AFN", "EUR", "ALL", "DZD", "USD", "AOA", "XCD", "ARS", "AMD", "AWG", "AUD", "AZN", "BSD", "BHD", "BDT", "BBD", "BYN", "BZD", "XOF", "BMD", "INR", "BTN"};
+    String[] country = {"EUR", "USD", "GBP", "KWD", "INR", "CAD", "JPY", "ALL", "ARs", "AUD", "EGP"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -127,39 +119,177 @@ public class FXConverterActivity extends AppCompatActivity {
             public void onClick(View v) {
                 try {
                     Double amountToConvert = Double.valueOf(FXConverterActivity.this.amountToConvert.getText().toString());
-                    getConversionRate(convertFromValue, convertToValue, amountToConvert);
+                    //getConversionRate(convertFromValue, convertToValue, amountToConvert);
+                    convertCurrency();
                 }catch (Exception e){}
             }
         });
     }
-    public String getConversionRate(String convertFrom, String convertTo, Double amountToConvert){
-        RequestQueue queue = Volley.newRequestQueue(this);
-        String url = "https://free.currconv.com/api/v7/convert?q=" + convertFrom + "_" + convertTo + "&compact=ultra&apiKey=";
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
-            @Override
-            public void onResponse(String response) {
-                JSONObject jsonObject = null;
-                try {
-                    jsonObject = new JSONObject(response);
-                    Double conversionRateValue = round(((Double) jsonObject.get(convertFrom + "_" + convertTo)), 2);
-                    conversionValue = "" + round((conversionRateValue * amountToConvert), 2);
-                    conversionRateText.setText(conversionValue);
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-            }
-            },new Response.ErrorListener(){
-                @Override
-                public void onErrorResponse(VolleyError error){}
-        });
-        queue.add(stringRequest);
-        return null;
+
+    private void convertCurrency() {
+        double conversionRate = getConversionRate(convertFromValue, convertToValue);
+        if (conversionRate != -1) {
+            double amount = Double.parseDouble(amountToConvert.getText().toString());
+            double convertedAmount = amount * conversionRate;
+            conversionValue = String.valueOf(round(convertedAmount, 2));
+            conversionRateText.setText(conversionValue);
+        } else {
+        }
     }
 
-    public static double round(double value, int places){
-        if(places<0) throw new IllegalArgumentException();
-        BigDecimal bigDecimal = BigDecimal.valueOf(value);
-        bigDecimal = bigDecimal.setScale(places, RoundingMode.HALF_UP);
-        return bigDecimal.doubleValue();
+    private double getConversionRate(String convertFrom, String convertTo) {
+        return switch (convertFrom) {
+            case "EUR" -> switch (convertTo) {
+                case "USD" -> 1.18;
+                case "GBP" -> 0.85;
+                case "KWD" -> 0.36;
+                case "INR" -> 88.08;
+                case "CAD" -> 1.46;
+                case "JPY" -> 129.98;
+                case "ALL" -> 123.62;
+                case "ARS" -> 115.97;
+                case "AUD" -> 1.57;
+                case "EGP" -> 18.56;
+                default -> -1;
+            };
+            case "USD" -> switch (convertTo) {
+                case "EUR" -> 0.85;
+                case "GBP" -> 0.72;
+                case "KWD" -> 0.30;
+                case "INR" -> 73.97;
+                case "CAD" -> 1.25;
+                case "JPY" -> 111.72;
+                case "ALL" -> 106.59;
+                case "ARS" -> 100.30;
+                case "AUD" -> 1.36;
+                case "EGP" -> 16.09;
+                default -> -1;
+            };
+            case "GBP" -> switch (convertTo) {
+                case "EUR" -> 1.18;
+                case "USD" -> 1.39;
+                case "KWD" -> 0.42;
+                case "INR" -> 103.50;
+                case "CAD" -> 1.72;
+                case "JPY" -> 152.55;
+                case "ALL" -> 144.82;
+                case "ARS" -> 136.49;
+                case "AUD" -> 1.85;
+                case "EGP" -> 21.83;
+                default -> -1;
+            };
+            case "KWD" -> switch (convertTo) {
+                case "EUR" -> 2.77;
+                case "USD" -> 3.31;
+                case "GBP" -> 2.38;
+                case "INR" -> 251.62;
+                case "CAD" -> 4.13;
+                case "JPY" -> 366.84;
+                case "ALL" -> 349.10;
+                case "ARS" -> 328.92;
+                case "AUD" -> 4.46;
+                case "EGP" -> 52.68;
+                default -> -1;
+            };
+            case "INR" -> switch (convertTo) {
+                case "EUR" -> 0.011;
+                case "USD" -> 0.014;
+                case "GBP" -> 0.0096;
+                case "KWD" -> 0.004;
+                case "CAD" -> 0.018;
+                case "JPY" -> 1.61;
+                case "ALL" -> 1.53;
+                case "ARS" -> 1.44;
+                case "AUD" -> 0.019;
+                case "EGP" -> 0.22;
+                default -> -1;
+            };
+            case "ALL" -> switch (convertTo) {
+                case "EUR" -> 0.0084;
+                case "USD" -> 0.01;
+                case "GBP" -> 0.007;
+                case "KWD" -> 0.003;
+                case "INR" -> 0.69;
+                case "CAD" -> 0.012;
+                case "JPY" -> 1.05;
+                case "ARS" -> 0.94;
+                case "AUD" -> 0.013;
+                case "EGP" -> 0.15;
+                default -> -1;
+            };
+            case "ARS" -> switch (convertTo) {
+                case "EUR" -> 0.012;
+                case "USD" -> 0.014;
+                case "GBP" -> 0.01;
+                case "KWD" -> 0.004;
+                case "INR" -> 0.87;
+                case "CAD" -> 0.013;
+                case "JPY" -> 1.12;
+                case "ALL" -> 1.06;
+                case "AUD" -> 0.014;
+                case "EGP" -> 0.17;
+                default -> -1;
+            };
+            case "AUD" -> switch (convertTo) {
+                case "EUR" -> 0.58;
+                case "USD" -> 0.69;
+                case "GBP" -> 0.50;
+                case "KWD" -> 0.21;
+                case "INR" -> 46.25;
+                case "CAD" -> 0.92;
+                case "JPY" -> 81.88;
+                case "ALL" -> 78.11;
+                case "ARS" -> 73.28;
+                case "EGP" -> 8.63;
+                default -> -1;
+            };
+            case "EGP" -> switch (convertTo) {
+                case "EUR" -> 0.046;
+                case "USD" -> 0.055;
+                case "GBP" -> 0.039;
+                case "KWD" -> 0.016;
+                case "INR" -> 3.52;
+                case "CAD" -> 0.078;
+                case "JPY" -> 7.10;
+                case "ALL" -> 6.75;
+                case "ARS" -> 6.37;
+                case "AUD" -> 0.12;
+                default -> -1;
+            };
+            case "CAD" -> switch (convertTo) {
+                case "EUR" -> 0.68;
+                case "USD" -> 0.81;
+                case "GBP" -> 0.58;
+                case "KWD" -> 0.24;
+                case "INR" -> 52.94;
+                case "JPY" -> 89.35;
+                case "ALL" -> 84.95;
+                case "ARS" -> 80.11;
+                case "AUD" -> 1.09;
+                case "EGP" -> 12.87;
+                default -> -1;
+            };
+            case "JPY" -> switch (convertTo) {
+                case "EUR" -> 0.0077;
+                case "USD" -> 0.0092;
+                case "GBP" -> 0.0066;
+                case "KWD" -> 0.0027;
+                case "INR" -> 0.62;
+                case "CAD" -> 0.011;
+                case "ALL" -> 0.95;
+                case "ARS" -> 0.89;
+                case "AUD" -> 0.012;
+                case "EGP" -> 0.14;
+                default -> -1;
+            };
+            default -> -1;
+        };
+    }
+
+    private double round(double value, int places) {
+        if (places < 0) throw new IllegalArgumentException();
+        BigDecimal bd = BigDecimal.valueOf(value);
+        bd = bd.setScale(places, RoundingMode.HALF_UP);
+        return bd.doubleValue();
     }
 }
